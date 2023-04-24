84361e46a563f7222bbf2a5d1d8f65b2d80aabf0
jonishall <joni.hallivuori@gmail.com>
2023-04-24 17:51:08 +0300

    Updated the backend server.js file to test some different api calls. The database was remade into one single 150MB file, and will later be added back as a script that will download the required files and recreate the database. For now, you can build "citybikedata.db" by just importing stations as a table with the same name and all 3 journey*.db files into a table named journeys.

--- cityBikeBackend/server.js
+++ cityBikeBackend/server.js
@@ -1,20 +1,17 @@
 const express = require('express');
 const app = express();
 const sqlite3 = require('sqlite3').verbose();
-const stationsdata = new sqlite3.Database('./stations.db');
-const journeys1 = new sqlite3.Database('./journeys1.db');
-const journeys2 = new sqlite3.Database('./journeys2.db');
-const journeys3 = new sqlite3.Database('./journeys3.db');
+const citybikedata = new sqlite3.Database('./citybikedata.db');
 const cors = require('cors');
 app.use(cors());
 
 //initializing everything required, the backend uses express and sqlite3.
 
 
-// an api that returns every station from the database with some error handling
+// an api call that sends every station from the database. Small enough data set to send everything.
 app.get('/api/stations', (req, res) => {
-  stationsdata.all('SELECT * FROM stations', (err, rows) => {
-    
+  citybikedata.all('SELECT * FROM stations', (err, rows) => {
+
     if (err) {
       console.error(err.message);
       res.status(500).send('Internal server error');
@@ -28,42 +25,59 @@ app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
 
-// an api that returns the output of "SELECT * FROM journeys WHERE Did=<:Did>;" from all 3 databases.
-// this is a placeholder method, it will change.
+// an api call that sends page <:Page> of journeys, sorted by departure station name and return station name.
 
-app.get('/api/id/:Did', (req, res) => {
-  const did=req.params.Did;
+app.get('/api/journeys/:Page', (req, res) => {
+  const start=(req.params.Page-1)*100;
+  citybikedata.all(`SELECT * FROM journeys ORDER BY Dname, Rname LIMIT 100 OFFSET ?`,start, (err, rows) => {
+
+    if (err) {
+      console.error(err.message);
+      res.status(500).send('Internal server error');
+    } else {
+      res.send(rows);
+   }
+  });
+});
+
+//api call to search for a particular station as departure or return station, case insensitive search.
+app.get('/api/search/:Search', (req, res) => {
+  const search='%'+req.params.Search+'%'
+  citybikedata.all(`SELECT * FROM journeys WHERE Dname like ? COLLATE NOCASE OR Rname like ? COLLATE NOCASE ORDER BY Dname, Rname`, search, search, (err, rows) => {
+
+    if (err) {
+      console.error(err.message);
+      res.status(500).send('Internal server error');
+    } else {
+      res.send(rows);
+   }
+  });
+});
+
+//api call to count journeys from and to a particular station
+app.get('/api/count/:Station', (req, res) => {
   result=[];
-  journeys1.all(`SELECT * FROM journeys where Did=?`,did, (err, rows) => {
-    
+  const station=req.params.Station
+  citybikedata.all(`SELECT COUNT(*) FROM journeys WHERE Dname=? COLLATE NOCASE`, station, (err, amount) => {
     if (err) {
       console.error(err.message);
       res.status(500).send('Internal server error');
+      result=[];
     } else {
-      result.push(rows);
-   journeys2.all(`SELECT * FROM journeys where Did=?`,did, (err, rows) => {
-    
-    if (err) {
-      console.error(err.message);
-      res.status(500).send('Internal server error');
-    } else {
-      result.push(rows);
-   journeys3.all(`SELECT * FROM journeys where Did=?`,did, (err, rows) => {
-    
-    if (err) {
-      console.error(err.message);
-      res.status(500).send('Internal server error');
-    } else {
-      result.push(rows);
-      res.send(result);
-      
-
-    }
-  });
-
-    }
-  });
-
+      result.push(amount);
+      citybikedata.all(`SELECT COUNT(*) FROM journeys WHERE Rname=? COLLATE NOCASE`, station, (err, amount) => {
+        if (err) {
+          console.error(err.message);
+          res.status(500).send('Internal server error');
+          result=[];
+        } else {
+          result.push(amount);
+          res.send(result);
+          result=[];
+        }
+      });
     }
   });
 });
+
+//these api's were created to test things out. I'm probably going to create one "master" api that can do all of these and more.
