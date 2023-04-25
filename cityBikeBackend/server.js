f582be241ea25db19de782d26d43fbf91d95b1f5
jonishall <joni.hallivuori@gmail.com>
2023-04-25 20:39:31 +0300

    Sanitized user input from sql calls.

--- cityBikeBackend/server.js
+++ cityBikeBackend/server.js
@@ -11,14 +11,15 @@ app.use(cors());
 // an api call that sends every station from the database. Small enough data set to send everything.
 // if stationName parameter is included, will instead send only stations that match the search.
 app.get('/api/stations', (req, res) => {
-  let query = `SELECT * FROM stations`
+  let query = `SELECT * FROM stations`;
+  let param=[];
   // check for additional query parameters
   if (req.query.hasOwnProperty('stationName')) {
-    query += ` WHERE Dname LIKE '%${req.query.stationName}%' COLLATE NOCASE OR Rname LIKE '%${req.query.stationName}%' COLLATE NOCASE`;
+    query += ` WHERE Dname LIKE ? COLLATE NOCASE OR Rname LIKE ? COLLATE NOCASE`;
+    param=[`%${req.query.stationName}%`,`%${req.query.stationName}%`];
   }
   else{}
   citybikedata.all(query, (err, rows) => {
-
     if (err) {
       console.error(err.message);
       res.status(500).send('Internal server error');
@@ -31,8 +32,9 @@ app.get('/api/stations', (req, res) => {
 
 
 // master api call that can take a few parameters. Parameters are station name, ordering and page number.
-
+// sanitized inputs using '?' instead of just string construction
 app.get('/api/journeys', (req, res) => {
+  let param=[];
   let query= `SELECT * FROM journeys`;
   // check for additional query parameters
   if (req.query.hasOwnProperty('count')) {
@@ -40,22 +42,29 @@ app.get('/api/journeys', (req, res) => {
   }
   if (req.query.hasOwnProperty('stationName') && req.query.hasOwnProperty('count')) {
     // if we're counting outbound and inbound journeys from a certain station, we need both cases in separate queries.
-    query+= ` WHERE Dname LIKE '%${req.query.stationName}%' COLLATE NOCASE UNION ALL SELECT COUNT(*) FROM journeys WHERE Rname LIKE '%${req.query.stationName}%' COLLATE NOCASE`;
+    query+= ` WHERE Dname LIKE ? COLLATE NOCASE UNION ALL SELECT COUNT(*) FROM journeys WHERE Rname LIKE ? COLLATE NOCASE`;
+    param=[`%${req.query.stationName}%`,`%${req.query.stationName}%`];
   }
   else if (req.query.hasOwnProperty('stationName') && !req.query.hasOwnProperty('count')) {
     // if not counting, proceed normally.
-    query += ` WHERE Dname LIKE '%${req.query.stationName}%' COLLATE NOCASE OR Rname LIKE '%${req.query.stationName}%' COLLATE NOCASE`;
+    query += ` WHERE Dname LIKE ? COLLATE NOCASE OR Rname LIKE ? COLLATE NOCASE`;
+    param=[`%${req.query.stationName}%`,`%${req.query.stationName}%`]
   }
   else{}
+
+  // using '?' substitution here doesn't work, because sqlite is stupid. handling it differently.
   if (req.query.hasOwnProperty('orderBy') && !req.query.hasOwnProperty('count')) {
-    query += ` ORDER BY ${req.query.orderBy}`;
+    if(['Departure', 'Return', 'Did', 'Dname', 'Rid', 'Rname', 'Distance', 'Duration'].includes(req.query.orderBy)){
+      query += ` ORDER BY ${req.query.orderBy}`;
+    }
   }
   else{}
 
   if (req.query.hasOwnProperty('pageNum') && !req.query.hasOwnProperty('count')) {
     // same as below but now we get the pageNum'th page.
     const offset = (parseInt(req.query.pageNum) - 1) * 100;
-    query += ` LIMIT 100 OFFSET ${offset}`;
+    query += ` LIMIT 100 OFFSET ?`;
+    param.push(offset);
   }
   else if(!req.query.hasOwnProperty('count')){
     // if not counting, we dont want to choke the browser with a million lines, so we get the first 100.
@@ -63,10 +72,9 @@ app.get('/api/journeys', (req, res) => {
   }
   else{}
 
-  citybikedata.all(query, (err, rows) => {
+  citybikedata.all(query, param, (err, rows) => {
     if (err) {
       console.error(err.message);
-      console.log(query)
       res.status(500).send('Internal server error');
     } else {
       res.send(rows);
